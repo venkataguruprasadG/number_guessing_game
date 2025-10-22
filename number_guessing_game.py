c27f0f59d2033abab2ee557b9b5f709d9e97d779
venkataguruprasadG <venkatagp27@gmail.com>
2025-10-22 09:37:31 +0530

    initial phase game design.

---
+++ number_guessing_game.py
@@ -0,0 +1,29 @@
+import random
+print("Welcome to number guessing game.")
+
+print("I've picked a number for you to guess.")
+print("The number is between 1 and 5.")
+
+computer_num_guess = random.randint(1,5)
+
+while True:
+
+    user_num_guess = int(input("Guess a number: "))
+
+    if user_num_guess == computer_num_guess:
+        print("Correct guess.")
+        print("You won. Thank you for playing.")
+        break
+    else:
+        print("your guess is not correct.")
+        print("GIve it another shot.")
+        print("Choose a higher number.")
+
+        user_num_guess2 = int(input("Enter guess number again: "))
+
+        if user_num_guess2 == computer_num_guess:
+            print("Correct guess.")
+            print("You won. Thank you for playing.")
+            break
+
+    
