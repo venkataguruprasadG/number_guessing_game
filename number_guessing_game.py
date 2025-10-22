02c9ff08cc42fdf6218fd09dd2539c0c7f889bd6
venkataguruprasadG <venkatagp27@gmail.com>
2025-10-22 10:26:50 +0530

    fixed small bugs.

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -1,13 +1,12 @@
 import random
-print("Welcome to number guessing game.")
 
+print("Welcome to number guessing game.")
 print("I've picked a number for you to guess.")
 print("The number is between 1 and 5.")
 
-computer_num_guess = random.randint(1,5)
+computer_num_guess = random.randint(1, 5)
 
 while True:
-
     user_num_guess = int(input("Guess a number: "))
 
     if user_num_guess == computer_num_guess:
@@ -15,9 +14,9 @@ while True:
         print("You won. Thank you for playing.")
         break
     else:
-        print("your guess is not correct.")
-        print("GIve it another shot.")
-        print("Choose a another number.")
+        print("Your guess is not correct.")
+        print("Give it another shot.")
+        print("Choose another number.")
 
         user_num_guess2 = int(input("Enter guess number again: "))
 
@@ -25,5 +24,3 @@ while True:
             print("Correct guess.")
             print("You won. Thank you for playing.")
             break
-
-    
